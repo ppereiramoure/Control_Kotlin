6949b04f55172b30425c0fcc254d904f3bfe62db
Pablo Pereira Moure <ppereiramoure@danielcastelao.com>
2021-05-14 10:03:17 +0200

    Ejercicio 2

---
+++ src/main/kotlin/ejercicio2.kt
@@ -0,0 +1,7 @@
+    class ejercicio2(tamaño: Int, rellenar: (Int) -> Unit){
+
+        val array = Array(
+            tamaño,
+            rellenar
+        )
+    }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,10 @@
-fun main(args: Array<String>) {
+import kotlin.random.Random
 
-        for (numero in ArrayClass(11).array){
-            println(numero)
-        }
+fun main(args: Array<String>) {
+    for (iterador in ArrayClass(11).array){
+        println(iterador)
     }
+    val array = ejercicio2(10){ Random.nextInt(50, 250)}
+
+    array.array.forEach { println(it) }
+}
